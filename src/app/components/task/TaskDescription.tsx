6b01461eb6c7ed98ef5c7fbb00e3a293d46135f6
prot-alx <prot.alx.92@gmail.com>
2024-12-14 23:05:45 +0500

    taskdescription edit

--- src/app/components/task/TaskDescription.tsx
+++ src/app/components/task/TaskDescription.tsx
@@ -1,26 +1,18 @@
 "use client";
-import { memo } from "react";
 import { Card, CardHeader, Alert } from "../ui";
 import { TASK_DESCRIPTION } from "@/constants";
 
-// пришлось мемоизировать, иначе на мобильных устройствах страдала производительность, индекс LCP был красным
-const TaskContent = memo(function TaskContent() {
-  return (
-    <Alert role="region" aria-label="Описание задачи">
-      {TASK_DESCRIPTION.content}
-    </Alert>
-  );
-});
-
-export const TaskDescription = memo(function TaskDescription() {
+export const TaskDescription = () => {
   return (
     <section>
       <Card>
         <CardHeader>
           <h2 className="text-xl font-bold">{TASK_DESCRIPTION.title}</h2>
         </CardHeader>
-        <TaskContent />
+        <Alert role="region" aria-label="Описание задачи">
+          {TASK_DESCRIPTION.content}
+        </Alert>
       </Card>
     </section>
   );
-});
+};
